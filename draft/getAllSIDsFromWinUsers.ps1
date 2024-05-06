f4ab7d8c92ab9f3fcff00c95657e97f8b5e999c2
Vítor Reiter <vitor.akr@gmail.com>
2024-05-06 10:03:29 -0300

    Adding scripts to get all SIDs/users and to change automatically hostname for everyone
    The Security Identifier (SID) is a unique name (an alphanumeric character string) that is assigned by a Windows Domain controller during the login process that is used to identify a user. Every user has your own SID in a folder from the Windows Registry Editor.

---
+++ draft/getAllSIDsFromWinUsers.ps1
@@ -0,0 +1,21 @@
+﻿
+#Discover the SIDs from user
+$newLabelnName = hostname
+
+#create SID array
+$usersSID = @()
+$usersSID += Get-ChildItem "hklm:software/microsoft/windows nt/currentversion/profilelist/S-1-5-21-*" | % pschildname
+
+$winUsers = @()
+$winUsers += (Get-ItemProperty -Path 'hklm:software/microsoft/windows nt/currentversion/profilelist/S-1-5-21-*' | % ProfileImagePath).ToUpper().Trim('C:\USERS\')
+
+
+    Write-Host "Array SID created with size of"$usersSID.length
+    Write-Host "Array Win-Users created with size of"$winUsers.length
+    Write-Host "Nome do PC"$newLabelnName
+	
+   foreach ($user in $winUsers){
+        #Discover the SID from user
+        $sidUser = ls 'hklm:software/microsoft/windows nt/currentversion/profilelist' | ? {$_.getvalue('profileimagepath') -match $user} | % pschildname
+        Write-Host "The user:"$user " - has the following SID defined:"$sidUser
+    }
---
+++ updateComputerNameForAllUsers/registryComputerNameValue_automated.ps1
@@ -0,0 +1,42 @@
+﻿
+#Check if the user running the script has Administrator rights
+IF (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+    [Security.Principal.WindowsBuiltInRole] “Administrator”))
+{
+
+    Write-Warning “You DONT have Administrator rights to run this script!`nPlease re-run this script as an Administrator!”
+    Break
+
+}
+#Insert user
+#$userEMEA = Read-Host -Prompt 'Write your EMEA account name (value like A[numbers])'
+#Create array containing all the users
+$winUsers = @()
+$winUsers += (Get-ItemProperty -Path 'hklm:software/microsoft/windows nt/currentversion/profilelist/S-1-5-21-*' | % ProfileImagePath).ToUpper().Trim('C:\USERS\')
+
+#Inform label name value
+#$newLabelName = Read-Host -Prompt 'Write your desired computer name label'
+$newLabelName = hostname
+
+foreach ($user in $winUsers){
+        #Discover the SID from user
+        $sidUser = ls 'hklm:software/microsoft/windows nt/currentversion/profilelist' | ? {$_.getvalue('profileimagepath') -match $user} | % pschildname
+        #Write-Host "The SID is:"$sidUser
+
+        #Set registry path location
+        $regPathValue ="Registry::HKU\$sidUser\Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{20D04FE0-3AEA-1069-A2D8-08002B30309D}"
+
+        #Check if this path exists (if yes, means that the user logged recently or is already logged in)
+    IF(Test-Path $regPathValue){
+        #Write-Host "Path exists! PATH:"$regPathValue 
+        
+            Write-Host -ForegroundColor DarkGreen ".... Changing computer name label for user:"$user
+        #Set new label computer name
+            Set-ItemProperty -Path $regPathValue -Name "(default)" -Value $newLabelName
+
+        #Show values under path
+            Get-ItemProperty -Path $regPathValue | Select-Object "(default)", PSPath | Format-List
+        } ELSE {
+        Write-Host -BackgroundColor DarkRed -ForegroundColor Yellow "The user $user did NOT have the computer name changed (due to not being logged in or their session being lost)."`n
+        }
+}
